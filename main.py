fa8c33ce7ca6367f51230c5419359fb14fcff3c2
user <user@>
2025-01-29 15:11:16 +0600

    second assignment

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+from person import Person
+
+
+
+
+
+if __name__ == '__main__':
+    person1 = Person('Aaaa', 'Bbbb', age=25)
+    person1.info()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
